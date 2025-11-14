c7513a6293bbf267f6ee9cfcdc4d16e17907d047
Neda2017 <107911909+Neda2017@users.noreply.github.com>
2025-11-14 17:48:03 +0200

    update

--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
+app.use(express.static("public"));
 
 // ✅ HEALTH CHECK
 app.get("/", (req, res) => {
